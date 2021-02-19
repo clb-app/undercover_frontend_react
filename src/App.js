3e60d277445ef1c43aa03e686118685b074c8456
lucas-lmk <ltidaspro@gmail.com>
2021-02-19 16:07:38 +0100

    navigation created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,41 @@
+// import des packages
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+// import CSS
 import "./App.css";
 
+// import des containers
+import Home from "./containers/Home";
+import NewParty from "./containers/NewParty";
+import JoinParty from "./containers/JoinParty";
+import Party from "./containers/Party";
+
+// import des composants
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
 const App = () => {
-  return <div className="App">TEST</div>;
+  return (
+    <Router>
+      <Header />
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+        <Route path="/new">
+          <NewParty />
+        </Route>
+        <Route path="/join">
+          <JoinParty />
+        </Route>
+        <Route path="/party/:code">
+          <Party />
+        </Route>
+      </Switch>
+      <Footer />
+    </Router>
+  );
 };
 
 export default App;
