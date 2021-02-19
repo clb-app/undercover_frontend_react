55463efbd575dd837afba0cc420931d81d8ad3d9
lucas-lmk <ltidaspro@gmail.com>
2021-02-19 16:22:16 +0100

    Input component created

---
+++ src/components/Input.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Input = ({ label, type, placeholder }) => {
+  return (
+    <div>
+      <label>{label}</label>
+      <input type={type} placeholder={placeholder} />
+    </div>
+  );
+};
+
+export default Input;
