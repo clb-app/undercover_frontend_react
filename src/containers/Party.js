43f6e788538073c0a12a61d26f8a441848493ec2
lucas-lmk <ltidaspro@gmail.com>
2021-03-01 17:33:22 +0100

    playing tour added

--- src/containers/Party.js
+++ src/containers/Party.js
@@ -8,6 +8,7 @@ import "./Party.css";
 
 // import des composants
 import Button from "../components/Button";
+import Input from "../components/Input";
 
 const Party = ({ player, api, token }) => {
   const { code } = useParams();
@@ -16,6 +17,10 @@ const Party = ({ player, api, token }) => {
   const [isLoading, setIsLoading] = useState(true);
   const [party, setParty] = useState(null);
   const [playersNumber, setPlayersNumber] = useState(1);
+  const [isPartyStarted, setIsPartyStarted] = useState(false);
+  const [playerPlaying, setPlayerPlaying] = useState(null);
+  const [input, setInput] = useState("");
+  const [previousPlay, setPreviousPlay] = useState(null);
 
   const socket = socketClient(api, { transports: ["websocket"] });
 
@@ -32,7 +37,21 @@ const Party = ({ player, api, token }) => {
         }
       }
     });
-    socket.on("server-startParty", () => {});
+    socket.on("server-startParty", (party) => {
+      setParty(party);
+
+      for (let i = 0; i < party.players.length; i++) {
+        if (!party.players[i].isAlreadyPlayed) {
+          setPlayerPlaying(party.players[i]);
+          if (i > 0) {
+            setPreviousPlay(party.players[i - 1]);
+          }
+          break;
+        }
+      }
+
+      setIsPartyStarted(true);
+    });
   }, []);
 
   const goBackHome = () => {
@@ -43,38 +62,67 @@ const Party = ({ player, api, token }) => {
     socket.emit("startParty", code);
   };
 
-  console.log(player);
+  const handlePlay = () => {
+    socket.emit("client-play", input, playerPlaying);
+  };
 
   return (
     <div className="Party">
-      <div>
-        <Button title="Retour" onClick={goBackHome} />
-      </div>
-
-      {isLoading ? (
-        <div>Chargement</div>
-      ) : (
+      {isPartyStarted ? (
         <>
-          <div className="Party-code-container">
-            <div>Code de la partie : {party.code}</div>
-          </div>
-          <div className="Party-playersList-container">
-            <h2>Liste des joueurs :</h2>
-            <div>
-              {party.players.length} / {playersNumber} joueurs
-            </div>
-          </div>
-
           {party.players.map((player) => {
             return <div key={player._id}>{player.nickname}</div>;
           })}
-          {party.players.length === playersNumber && player ? (
-            // player &&
-            party.moderator_id === player._id && (
-              <Button title="Démarrer la partie" onClick={handleStartParty} />
-            )
+          {previousPlay && (
+            <div>
+              Dernier mot joué par {previousPlay.nickname} est{" "}
+              {previousPlay.words[0]}
+            </div>
+          )}
+          {playerPlaying._id === player._id ? (
+            <>
+              <Input label="Mot" placeholder="Ex: chien" setInput={setInput} />
+              <Button title="Jouer" onClick={handlePlay} />
+            </>
           ) : (
-            <div>En attente d'autres joueurs ...</div>
+            <div>{playerPlaying.nickname} est en train de jouer ...</div>
+          )}
+        </>
+      ) : (
+        <>
+          <div>
+            <Button title="Retour" onClick={goBackHome} />
+          </div>
+
+          {isLoading ? (
+            <div>Chargement</div>
+          ) : (
+            <>
+              <div className="Party-code-container">
+                <div>Code de la partie : {party.code}</div>
+              </div>
+              <div className="Party-playersList-container">
+                <h2>Liste des joueurs :</h2>
+                <div>
+                  {party.players.length} / {playersNumber} joueurs
+                </div>
+              </div>
+
+              {party.players.map((player) => {
+                return <div key={player._id}>{player.nickname}</div>;
+              })}
+              {party.players.length === playersNumber && player ? (
+                // player &&
+                party.moderator_id === player._id && (
+                  <Button
+                    title="Démarrer la partie"
+                    onClick={handleStartParty}
+                  />
+                )
+              ) : (
+                <div>En attente d'autres joueurs ...</div>
+              )}
+            </>
           )}
         </>
       )}
