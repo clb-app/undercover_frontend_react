b1a718147412f6ec85066a4d20fe685faeb5aa37
lucas-lmk <ltidaspro@gmail.com>
2021-04-05 08:39:49 +0200

    LaunchParty-screen created and Home screen updated

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,9 +3,9 @@ import React from "react";
 // import CSS
 import "./Button.css";
 
-const Button = ({ title, onClick, width }) => {
+const Button = ({ title, onClick, width, bgcColor }) => {
   return (
-    <button onClick={onClick} style={{ width }}>
+    <button onClick={onClick} style={{ width, backgroundColor: bgcColor }}>
       {title}
     </button>
   );
